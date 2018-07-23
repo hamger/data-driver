536ea77cd6bf8b74a8c6a95bc95a3e78d10cb42a
hamger <2313303800@qq.com>
2018-07-23 14:57:40 +0800

    add simple usage

--- README.md
+++ README.md
@@ -1,6 +1,24 @@
 # data-driver
 a data driver with javascript
 
+## Usage
+```js
+var demo = new DD({
+  data () {
+    return {
+      text: 'hello world'
+    }
+  }
+})
+
+demo.$watch('text', (val, oldVal) => {
+  console.log(`text的值从 ${oldVal} 变更为 ${val}`)
+})
+
+demo.text = 'hello data-dirver'
+// text的值从 hello world 变更为 hello data-dirver
+```
+
 ## Changelog
 ## 2018.7.22
 > v0.2.1 修复父类配置项指向的错误和未合并所有配置项的错误
--- demo/index.js
+++ demo/index.js
@@ -12,56 +12,55 @@ window.onload = function () {
   DD.$mount(document.getElementById('app'), App)
 }
 
-// // 使用基础 Amus 构造器，创建一个“子类”。参数是一个包含组件选项的对象。
 // let childConstructor = DD.extend({
-//     data() {
-//         return {
-//             dataTest: 1
-//         }
-//     },
-//     methods: {
-//         methodTest() {
-//             console.log('methodTest')
-//         }
-//     },
-//     watch: {
-//         'dataTest'(newValue, oldValue) {
-//             console.log('watchTest newValue = ' + newValue)
-//         }
-//     },
-//     computed: {
-//         'computedTest': {
-//             get() {
-//                 return this.dataTest + 1
-//             }
-//         }
+//   data () {
+//     return {
+//       dataTest: 1
 //     }
+//   },
+//   methods: {
+//     methodTest () {
+//       console.log('methodTest')
+//     }
+//   },
+//   watch: {
+//     dataTest (newValue, oldValue) {
+//       console.log('watchTest newValue = ' + newValue)
+//     }
+//   },
+//   computed: {
+//     computedTest: {
+//       get () {
+//         return this.dataTest + 1
+//       }
+//     }
+//   }
 // })
 
 // // 实例化一个子组件
 // let child = new childConstructor({
-//     data() {
-//         return {
-//             subData: 11
-//         }
-//     },
-//     methods: {
-//         subMethod() {
-//             console.log('subMethodTest')
-//         }
-//     },
-//     watch: {
-//         'subData'(newValue, oldValue) {
-//             console.log('subWatch newValue = ' + newValue)
-//         }
-//     },
-//     computed: {
-//         'subComputed': {
-//             get() {
-//                 return this.subData + 1
-//             }
-//         }
+//   data () {
+//     return {
+//       subData: 11
 //     }
+//   },
+//   methods: {
+//     subMethod () {
+//       console.log('subMethodTest')
+//     }
+//   },
+//   watch: {
+//     subData (newValue, oldValue) {
+//       console.log('subWatch newValue = ' + newValue)
+//     }
+//   },
+//   computed: {
+//     subComputed: {
+//       get () {
+//         return this.subData + 1
+//       }
+//     }
+//   }
 // })
 
 // console.log(child.dataTest)
