cb104cf8b6bebed549c6c2941b30230d2224cdca
hamger <2313303800@qq.com>
2018-08-18 13:47:50 +0800

    0.2.4

--- demo/index.js
+++ demo/index.js
@@ -22,14 +22,14 @@ demo.$watch(
     return this.num1 + this.num2
   },
   (val, oldVal) => {
-    console.log(`num1和num2的和从 ${oldVal} 变更为 ${val}`)
+    console.log(`num1 与 num2 的和从 ${oldVal} 变更为 ${val}`)
   }
 )
 
 demo.num1++
-// num1和num2的和从 5 变更为 6
+// num1 与 num2 的和从 5 变更为 6
 demo.num2--
-// num1和num2的和从 5 变更为 6
+// num1 与 num2 的和从 5 变更为 6
 
 var obj = { count1: 1, count2: 2 }
 observe(obj)
@@ -39,11 +39,11 @@ var watch = new Watcher(
     return obj.count1 + obj.count2
   },
   (val, oldVal) => {
-    console.log(`count1和count2的和从 ${oldVal} 变更为 ${val}`)
+    console.log(`count1 与 count2 的和从 ${oldVal} 变更为 ${val}`)
   }
 )
-obj.count1++
-obj.count2--
+obj.count1++ // count1 与 count2 的和从 3 变更为 4
+obj.count2-- // count1 与 count2 的和从 4 变更为 3
 watch.teardown()
 obj.count1++
 obj.count2--
